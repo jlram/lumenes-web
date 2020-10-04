3410dcee743fe153d5e9085fd80bfdeb08357a17
Victor Santaella <victorst79@gmail.com>
2020-10-04 14:32:55 +0200

    added new component and modules to about view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Component</h1>
   </div>
 </template>
+
+<script>
+
+export default {
+  name: 'About',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+  .about {
+    padding: 16px;
+    color: aquamarine;
+  }
+</style>
